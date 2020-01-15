647aeb3765a781fc009b99f54598c9d8172eaec3
fedelopezm <flopezm17@gmail.com>
2020-01-14 20:34:57 -0500

    the script was created and work, you just need to put the domain without www and .com

--- script.sh
+++ script.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 echo -m ".htaccess generator"
+
+read -p "Enter the website address without the www. and .com:  " URL
+
+
+touch ./.htaccess
+
+echo "RewriteEngine On 
+RewriteBase / 
+RewriteCond %{HTTPS} !=on 
+RewriteCond %{HTTP_HOST} ^$URL\.com$ [OR]
+RewriteCond %{HTTP_HOST} ^www\.$URL\.com$ 
+RewriteRule .* https://$URL.com%{REQUEST_URI} [R=301,L]" >> ./.htaccess
