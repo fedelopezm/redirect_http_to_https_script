fec53881138f75835df9a0ec70aac97f297f467e
fedelopezm <flopezm17@gmail.com>
2020-03-19 22:23:37 -0400

    add a last comment

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -m ".htaccess generator"
+echo ".htaccess generator"
 
 read -p "Enter the website address without the www. and .com:  " URL
 
@@ -13,3 +13,6 @@ RewriteCond %{HTTPS} !=on
 RewriteCond %{HTTP_HOST} ^$URL\.com$ [OR]
 RewriteCond %{HTTP_HOST} ^www\.$URL\.com$ 
 RewriteRule .* https://$URL.com%{REQUEST_URI} [R=301,L]" >> ./.htaccess
+
+echo "The file was created on the actual folder with the .htaccess name"
+echo "Upload this file on public_html and that's it"
